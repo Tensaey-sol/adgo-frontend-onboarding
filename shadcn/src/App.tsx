f5fbf644d54e02155dcbb696b309c0e0a95db65a
Tensaey Solomon <12tensaeysolomon3@gmail.com>
2024-10-28 21:15:37 +0300

    ShadCN setup

---
+++ shadcn/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <div className="p-5 m-5 items-center text-center">
+        <Button> Say Hi!</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
