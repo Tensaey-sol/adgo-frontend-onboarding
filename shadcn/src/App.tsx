b9cbf60d3711a54edb9bcabe2f0bca448e929f62
Tensaey Solomon <12tensaeysolomon3@gmail.com>
2024-10-28 21:40:46 +0300

    added another ShadCN component

--- shadcn/src/App.tsx
+++ shadcn/src/App.tsx
@@ -1,14 +1,27 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Calendar } from "./components/ui/calendar";
+import React from "react";
 
 function App() {
-
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
   return (
     <>
-      <div className="p-5 m-5 items-center text-center">
-        <Button> Say Hi!</Button>
+      <div className="p-5 m-5 text-center space-x-2">
+        <Button> Primary Style</Button>
+        <Button variant="destructive"> Destructive Style</Button>
+        <Button variant="outline"> Outline Style</Button>
+        <Button variant="ghost"> Ghost Style</Button>
+        <div className="flex justify-center mt-5 w-full">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            className="rounded-md border p-4 w-auto"
+          />
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
